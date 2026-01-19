0baa9c45251ac7c78166fcaf5932ed6ebdd34baa
Anthony Manuel Barrera Hidalgo <anthonybarreradev@gmail.com>
2026-01-19 18:07:17 -0400

    Agregando Main.java al proyecto

---
+++ Main.java
@@ -0,0 +1,7 @@
+package ejemplorepositorioprogramacion3;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola papasito");
+        System.out.println("Que hubo pues");
+    }
+}
